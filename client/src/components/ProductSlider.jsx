2216daaebed5212fa24dd98db6e512ad51f04717
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-11-05 17:59:00 +0200

    Added "ProductSlider" and "Product" components.
    Added slider, product information is taken from our Strapi admin panel. Also with the help of "Link" from "react-router-dom" add transition to the product page by card id.

--- client/src/components/ProductSlider.jsx
+++ client/src/components/ProductSlider.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React from 'react'
 
-const ProductSlider = () => {
-  return <div>ProductSlider</div>;
-};
+// Import Swiper React
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-export default ProductSlider;
+// Import Swiper React Styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import '../slider.css'
+
+// Import Required modules
+import { Pagination, Navigation } from 'swiper'
+
+// Import Components
+import Product from './Product';
+
+const ProductSlider = ({ data }) => {
+  return (
+    <Swiper
+      modules={[Pagination, Navigation]}
+      loop={false}
+      navigation={true}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        720: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        1440: {
+          slidesPerView: 5,
+          spaceBetween: 30
+        }
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      className='productSlider mx-auto max-w-[360px] md:max-w-lg xl:max-w-[1410px]'
+    >
+      <>
+        {data?.map(product => {
+          return (
+            <SwiperSlide key={product.id}>
+              <Product product={product} />
+            </SwiperSlide>
+          )
+        })}
+      </>
+    </Swiper>
+  )
+}
+
+export default ProductSlider

bd4da94bf81d5bc1b6f6b59ee3d8fc21f112f9c5
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-11-05 08:39:20 +0200

    Create Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React from 'react'
+
+// Import React-Router-DOM
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+// Import Pages
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+import Search from './pages/Search';
+import Products from './pages/Products';
+
+// Import Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Layout
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products/:id', element: <Products /> },
+      { path: '/product/:id', element: <ProductDetails /> },
+      { path: '/search', element: <Search /> },
+    ],
+  }
+])
 
 const App = () => {
-  return <div>App</div>;
-};
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
 
-export default App;
+export default App

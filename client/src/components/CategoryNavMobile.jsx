919f4571d14efbe4f1df614330c19c5b00cabf5b
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-11-06 21:04:08 +0200

    Styling of the "CategoryNavMobile" component.

--- client/src/components/CategoryNavMobile.jsx
+++ client/src/components/CategoryNavMobile.jsx
@@ -3,12 +3,38 @@ import React from 'react'
 // import Icons
 import { FiX } from 'react-icons/fi'
 
+// Import Link from React-Router-DOM
+import { Link } from 'react-router-dom'
+
+// Import UseFetch Hook
+import useFetch from './../hooks/useFetch';
+
 const CategoryNavMobile = ({ setCarNavMobile }) => {
+
+  // Get Categories
+  const { data } = useFetch('/categories')
+
   return (
     <div className='w-full h-full bg-primary p-8'>
       {/* Close Icon */}
-      <div onClick={() => setCarNavMobile(false)} className='flex justify-end mb-8 cursor-pointer'>
+      <div
+        onClick={() => setCarNavMobile(false)}
+        className='flex justify-end mb-8 cursor-pointer'
+      >
         <FiX className='text-3xl' />
+      </div>
+      <div className='flex flex-col gap-y-8'>
+        {data?.map(category => {
+          return (
+            <Link
+              className='uppercase font-medium'
+              key={category.id}
+              to={`products/${category.id}`}
+            >
+              {category.attributes.title} Cameras
+            </Link>
+          )
+        })}
       </div>
     </div>
   )

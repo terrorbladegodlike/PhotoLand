5198720387846b0350a1208e7ec34f63fe825915
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-11-07 20:43:40 +0200

    Adding "ProductDetails" and "RelatedProducts" component.
    Adding any product page and also in the "RelatedProducts" component shows more cameras of the same category, clicking on any of these cards shows the page of the already selected camera.

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-const RelatedProducts = () => {
-  return <div>RelatedProducts</div>;
-};
+// Import UseFetch Hook
+import useFetch from './../hooks/useFetch';
 
-export default RelatedProducts;
+// Import Components
+import ProductSlider from './ProductSlider';
+
+
+const RelatedProducts = ({ categoryTitle }) => {
+
+  // Get Product By Category Title
+  const { data } = useFetch(`products?populate=*&filters[categories][title]=${categoryTitle}`)
+
+  return (
+    <div className='mb-16'>
+      <div className="container mx-auto">
+        <h2 className='h2 mb-6 text-center xl:text-left'>
+          Related Product
+        </h2>
+        <ProductSlider data={data} />
+      </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -1,7 +1,78 @@
-import React from 'react';
+import React from 'react'
+
+// Import UseParams Hook from React-Router-DOM
+import { useParams } from 'react-router-dom';
+
+// Import UseFetch Hook
+import useFetch from './../hooks/useFetch';
+
+// Import Components
+import RelatedProducts from './../components/RelatedProducts';
+
+// Import Cart Context
+import { CartContext } from './../context/CartContext';
+
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
-};
 
-export default ProductDetails;
+  const { id } = useParams()
+
+  // Get Product Data Base On The ID
+  const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`)
+  if (!data) {
+    return (
+      <div className='container mx-auto'>
+        loading...
+      </div>
+    )
+  }
+
+  // Category Title
+  const categoryTitle = data[0].attributes.categories.data[0].attributes.title
+
+  return (
+    <div className='mb-16 pt-44 lg:pt-[30px] xl:pt-0'>
+      <div className="container mx-auto">
+        {/* Text */}
+        <div className='flex flex-col lg:flex-row gap-[30px] mb-[30px]'>
+          <div className='flex-1 lg:max-w-[40%] lg:h-[540px] grad rounded-lg flex justify-center items-center'>
+            <img
+              src={`http://localhost:1337${data[0].attributes.image.data.attributes.url}`}
+              alt="photography"
+              className='w-full max-w-[65%]'
+            />
+          </div>
+          <div className='flex-1 bg-primary p-12 xl:p-20 rounded-lg flex flex-col justify-center'>
+            {/* Category Title */}
+            <div className='uppercase text-accent text-lg font-medium mb-2'>
+              {data[0].attributes.categories.data[0].attributes.title} cameras
+            </div>
+            {/* Title */}
+            <h2 className='h2 mb-4'>
+              {data[0].attributes.title}
+            </h2>
+            {/* Description */}
+            <p className='mb-12'>
+              {data[0].attributes.description}
+            </p>
+            {/* Price & BTN */}
+            <div className='flex items-center gap-x-8'>
+              {/* Price */}
+              <div className='text-3xl text-accent font-semibold'>
+                ${data[0].attributes.price}
+              </div>
+              {/* BTN */}
+              <button className='btn btn-accent'>
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* Related Products */}
+        <RelatedProducts categoryTitle={categoryTitle} />
+      </div>
+    </div>
+  )
+}
+
+export default ProductDetails
